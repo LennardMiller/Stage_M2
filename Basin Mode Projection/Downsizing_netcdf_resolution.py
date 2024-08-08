448720fec499346901a2eb2c9ccfc7d597c5af0d
AlexandreMocean <alexandre421997@hotmail.fr>
2024-08-08 09:09:12 +0200

    le 8 aout

--- Basin Mode Projection/Downsizing_netcdf_resolution.py
+++ Basin Mode Projection/Downsizing_netcdf_resolution.py
@@ -1,7 +1,7 @@
 import xarray as xr
 
 # Open the original dataset with chunking (optional but helpful for large files)
-ds = xr.open_dataset('/home/massoale/Simu_Test/simu_dahu/simu_dahu922cd ../outdir_0001/vars.nc', chunks={'time': 1, 'y': 2049, 'x': 2049})
+ds = xr.open_dataset('/home/massoale/Simu_Test/simu_dahu/simu_dahu619/outdir_0001/vars.nc', chunks={'time': 1, 'y': 2049, 'x': 2049})
 
 # Downscale to 256x256 using coarsen
 ds_coarsened = ds.coarsen(x=8, y=8, boundary='trim').mean()
@@ -10,6 +10,6 @@ ds_coarsened = ds.coarsen(x=8, y=8, boundary='trim').mean()
 print(ds_coarsened)
 
 # Save the downscaled dataset
-ds_coarsened.to_netcdf('/home/massoale/Simu_Test/simu_dahu/simu_dahu922/outdir_0001/vars_256x256.nc')
+ds_coarsened.to_netcdf('/home/massoale/Simu_Test/simu_dahu/simu_dahu619/outdir_0001/vars_256x256.nc')
 
 
